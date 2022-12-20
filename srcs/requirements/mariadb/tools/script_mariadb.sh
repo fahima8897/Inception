70dcb941f37a268941ae840733d882baad19dbb8
Debian <debian@debian10.linuxvmimages.local>
2022-12-20 08:31:05 -0600

    mariadb

--- srcs/requirements/mariadb/tools/script_mariadb.sh
+++ srcs/requirements/mariadb/tools/script_mariadb.sh
@@ -12,5 +12,16 @@ mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$
 # USER PRIVILEGES
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# CHANGE ROOT USER
+# CHANGE ROOT PRIVILEGES
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# RELOAD DATA_BASE (this way modifications for user root are taken into account)
+mysql -e "FLUSH PRIVILEGES;"
+
+# SHUTDOWN MYSQL
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
+
+
+
